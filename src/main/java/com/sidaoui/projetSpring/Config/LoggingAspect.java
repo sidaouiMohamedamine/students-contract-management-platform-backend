c17ba23ade7f8b8027f14bd28d215804e1257f87
Sidaoui <mssidaoui@gmail.com>
2023-02-10 21:14:53 +0100

    Adding some advices

--- src/main/java/com/sidaoui/projetSpring/Config/LoggingAspect.java
+++ src/main/java/com/sidaoui/projetSpring/Config/LoggingAspect.java
@@ -3,8 +3,8 @@ package com.sidaoui.projetSpring.Config;
 
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -18,6 +18,29 @@ public class LoggingAspect {
             log.info("Début Execution : " + name + " : ");
         }
 
+    @AfterReturning("execution(* com.sidaoui.projetSpring.Service.*.*(..))")
+    public void logMethodExit1(JoinPoint joinPoint) {
+        String name = joinPoint.getSignature().getName();
+        log.info("Out of method without errors : " + name );
+    }
+    @AfterThrowing("execution(* com.sidaoui.projetSpring.Service.*.*(..))")
+    public void logMethodExit2(JoinPoint joinPoint) {
+        String name = joinPoint.getSignature().getName();
+        log.error("Out of method with erros : " + name );
+    }
+    @After("execution(* com.sidaoui.projetSpring.Service.*.*(..))")
+    public void logMethodExit(JoinPoint joinPoint) {
+        String name = joinPoint.getSignature().getName();
+        log.info("Out of method (in all cases) : " + name );
+    }
 
+    @Around("execution(* com.sidaoui.projetSpring.Service.*.*(..))")
+    public Object profile(ProceedingJoinPoint pjp) throws Throwable {
+        long start = System.currentTimeMillis();
+        Object obj = pjp.proceed();
+        long elapsedTime = System.currentTimeMillis() - start;
+        log.info("Method execution time: " + elapsedTime + " milliseconds.");
+        return obj;
+    }
 
 }
